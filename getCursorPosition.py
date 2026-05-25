1d59accaff8e5e3e902206a50d8d773d02f9895b
thiagocastro1721 <58869140+thiagocastro1721@users.noreply.github.com>
2026-05-24 22:27:59 -0300

    Delete getCursorPosition.py
    Não é mais necessário, pois a biblioteca pyautogui não será mais utilizada.

--- getCursorPosition.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-import time
-screenWidth, screenHeight = pyautogui.size()
-print(pyautogui.size())
-time.sleep(5)
-currentMouseX, curentMouseY = pyautogui.position()
-print(pyautogui.position())
